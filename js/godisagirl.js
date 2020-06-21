77c58c470d586bc7e82321347f6a289ad6afa864
tajiri <33457304+itstajiri@users.noreply.github.com>
2020-06-21 19:37:42 +0300

    Add files via upload

---
+++ js/godisagirl.js
@@ -0,0 +1,8 @@
+	var r_text = new Array ();
+			r_text[0] = "no one escapes the punishment of heaven";
+			r_text[1] = "god is a girl";
+			r_text[2] = "the day will come when the sky will turn bloody";
+			r_text[3] = "very soon the angels will descend from heaven";
+			r_text[4] = "gl hf";
+			var i = Math.floor(5*Math.random())
+			document.write(r_text[i]);
