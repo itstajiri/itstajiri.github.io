b1b3573f34d0c736ee63652aad651094f830b1fa
tajiri <33457304+itstajiri@users.noreply.github.com>
2026-06-20 10:37:24 +0300

    Update godisagirl.js

--- js/godisagirl.js
+++ js/godisagirl.js
@@ -1,7 +1,7 @@
 	var r_text = new Array ();
-			r_text[0] = "no one escapes the punishment of heaven";
+			r_text[0] = "no one escapes heaven's punishment";
 			r_text[1] = "god is a girl";
-			r_text[2] = "the day will come when the sky will turn bloody";
+			r_text[2] = "the day will come when the sky turns blood-red";
 			r_text[3] = "very soon the angels will descend from heaven";
 			r_text[4] = "gl hf";
 			r_text[5] = "the end is near";
